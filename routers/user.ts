49f75603a29e912e83154cd8564c1914dffbcde4
boojack <imrealleonardo@gmail.com>
2021-05-30 10:06:34 +0800

    -polish signup api disabled;

--- routers/user.ts
+++ routers/user.ts
@@ -8,6 +8,6 @@ export const userRouter = new Router({
 
 userRouter.get("/me", validSigninCookie, UserController.getMyUserInfo);
 userRouter.get("/amount", validSigninCookie, UserController.getDataAmount);
-userRouter.post("/signup", UserController.signup);
+// userRouter.post("/signup", UserController.signup);
 userRouter.post("/signin", UserController.signin);
 userRouter.post("/signout", UserController.signout);
