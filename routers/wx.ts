b15ce0cd974a9f0f3fd1f79089c0e581d4db4386
boojack <imrealleonardo@gmail.com>
2021-06-30 09:33:34 +0800

    -rm wx routers;

--- routers/wx.ts
+++
@@ -1,9 +0,0 @@
-import Router from "koa-router";
-import { WxController } from "../controllers/wx";
-
-export const wxRouter = new Router({
-  prefix: "/api/wx",
-});
-
-wxRouter.get("/", WxController.validate);
-wxRouter.get("/signs", WxController.getSignatures);
