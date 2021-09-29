12e0ade3347df9a4795dcbff8fd13e8ac85e39d9
LeeShuang <sli4@ebay.com>
2021-09-29 11:25:13 +0800

    -update db helper and file path;

--- helpers/DBHelper.ts
+++ helpers/DBHelper.ts
@@ -2,30 +2,38 @@ import { accessSync } from "fs";
 import sqlite3 from "sqlite3";
 import { utils } from "./utils";
 
-const defaultDbFile = "./db/memos.db";
-const userDbFile = "/data/memos.db";
+const defaultDbFile = "./data/memos.db";
+// JUST FOR DEV
+const devDbFile = "/Users/sli4/Downloads/data/memos.db";
+const userDbFile = process.env.NODE_ENV === "dev" ? devDbFile : "/data/memos.db";
 
-let temp: sqlite3.Database | null = null;
+function getDbInstance() {
+  let temp: sqlite3.Database;
 
-try {
-  accessSync(userDbFile);
-  temp = new sqlite3.Database(userDbFile, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Connected to the user database.");
-  });
-} catch (error) {
-  console.log("User db file not exist");
-  temp = new sqlite3.Database(defaultDbFile, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Connected to the default database.");
-  });
+  try {
+    accessSync(userDbFile);
+    temp = new sqlite3.Database(userDbFile, (err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log("Connected to the user database.");
+      }
+    });
+  } catch (error) {
+    console.log("User db file not exist");
+    temp = new sqlite3.Database(defaultDbFile, (err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log("Connected to the default database.");
+      }
+    });
+  }
+
+  return temp;
 }
 
-const db = temp as sqlite3.Database;
+let db = getDbInstance();
 
 function parseResult(result: any): BasicType {
   if (result instanceof Array) {
@@ -53,35 +61,50 @@ function parseResult(result: any): BasicType {
 
 export default {
   run: (sql: string, parms: any[]): Promise<boolean> => {
+    if (!db) {
+      db = getDbInstance();
+    }
     return new Promise((resolve, reject) => {
-      db.run(sql, parms, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(true);
-        }
+      db.serialize(() => {
+        db.run(sql, parms, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(true);
+          }
+        });
       });
     });
   },
   all: <T = any[]>(sql: string, parms: any[]): Promise<T> => {
+    if (!db) {
+      db = getDbInstance();
+    }
     return new Promise((resolve, reject) => {
-      db.all(sql, parms, (err, rows) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(parseResult(rows) as T);
-        }
+      db.serialize(() => {
+        db.all(sql, parms, (err, rows) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(parseResult(rows) as T);
+          }
+        });
       });
     });
   },
   get: <T = any>(sql: string, parms: any[]): Promise<T | null> => {
+    if (!db) {
+      db = getDbInstance();
+    }
     return new Promise((resolve, reject) => {
-      db.get(sql, parms, (err, row) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(parseResult(row) as T | null);
-        }
+      db.serialize(() => {
+        db.get(sql, parms, (err, row) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(parseResult(row) as T | null);
+          }
+        });
       });
     });
   },
