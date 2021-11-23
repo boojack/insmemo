62b9dcf7be6dcb294052d3faa879ea2e80ee0e69
LeeShuang <sli4@ebay.com>
2021-11-23 11:29:21 +0800

    -rm build files;

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ COPY . .
 
 RUN npm install --only=production
 
+RUN npm run build
+
 CMD [ "node", "./build/server.js" ]
 
 EXPOSE 8080
--- build/helpers/config-demo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.githubOAuthConfig = void 0;
-exports.githubOAuthConfig = {
-    clientId: "",
-    clientSecret: "",
-};
